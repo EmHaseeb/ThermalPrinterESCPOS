c9c945f986c462b4ba856abd03bcf2a362297077
EmHaseeb <emhaseeb65@gmail.com>
2023-05-29 18:41:30 +0500

    Image Gradient issue fixed and general alignment.

--- thermalPrinter/src/main/java/com/emh/thermalprinter/EscPosPrinterCommands.java
+++ thermalPrinter/src/main/java/com/emh/thermalprinter/EscPosPrinterCommands.java
@@ -1,7 +1,6 @@
 package com.emh.thermalprinter;
 
 import android.graphics.Bitmap;
-import android.util.Log;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
@@ -86,10 +85,10 @@ public class EscPosPrinterCommands {
 
     public static byte[] initGSv0Command(int bytesByLine, int bitmapHeight) {
         int
-                xH = bytesByLine / 256,
-                xL = bytesByLine - (xH * 256),
-                yH = bitmapHeight / 256,
-                yL = bitmapHeight - (yH * 256);
+            xH = bytesByLine / 256,
+            xL = bytesByLine - (xH * 256),
+            yH = bitmapHeight / 256,
+            yL = bitmapHeight - (yH * 256);
 
         byte[] imageBytes = new byte[8 + bytesByLine * bitmapHeight];
         imageBytes[0] = 0x1D;
@@ -107,35 +106,41 @@ public class EscPosPrinterCommands {
      * Convert Bitmap instance to a byte array compatible with ESC/POS printer.
      *
      * @param bitmap Bitmap to be convert
+     * @param gradient false : Black and white image, true : Grayscale image
      * @return Bytes contain the image in ESC/POS command
      */
-    public static byte[] bitmapToBytes(Bitmap bitmap) {
+    public static byte[] bitmapToBytes(Bitmap bitmap, boolean gradient) {
         int
-                bitmapWidth = bitmap.getWidth(),
-                bitmapHeight = bitmap.getHeight(),
-                bytesByLine = (int) Math.ceil(((float) bitmapWidth) / 8f);
+            bitmapWidth = bitmap.getWidth(),
+            bitmapHeight = bitmap.getHeight(),
+            bytesByLine = (int) Math.ceil(((float) bitmapWidth) / 8f);
 
         byte[] imageBytes = EscPosPrinterCommands.initGSv0Command(bytesByLine, bitmapHeight);
 
         int i = 8,
-                greyscaleCoefficientInit = 0,
-                gradientStep = 6;
+            greyscaleCoefficientInit = 0,
+            gradientStep = 6;
 
-        double colorLevelStep = 765.0 / (15 * gradientStep + gradientStep - 1);
+        double
+            colorLevelStep = 765.0 / (15 * gradientStep + gradientStep - 1);
+
         for (int posY = 0; posY < bitmapHeight; posY++) {
             int greyscaleCoefficient = greyscaleCoefficientInit,
-                    greyscaleLine = posY % gradientStep;
+                greyscaleLine = posY % gradientStep;
             for (int j = 0; j < bitmapWidth; j += 8) {
                 int b = 0;
                 for (int k = 0; k < 8; k++) {
                     int posX = j + k;
                     if (posX < bitmapWidth) {
                         int color = bitmap.getPixel(posX, posY),
-                                red = (color >> 16) & 255,
-                                green = (color >> 8) & 255,
-                                blue = color & 255;
+                            red = (color >> 16) & 255,
+                            green = (color >> 8) & 255,
+                            blue = color & 255;
 
-                        if ((red + green + blue) < ((greyscaleCoefficient * gradientStep + greyscaleLine) * colorLevelStep)) {
+                        if (
+                            (gradient && (red + green + blue) < ((greyscaleCoefficient * gradientStep + greyscaleLine) * colorLevelStep)) ||
+                                (!gradient && (red < 160 || green < 160 || blue < 160))
+                        ) {
                             b |= 1 << (7 - k);
                         }
 
@@ -159,17 +164,17 @@ public class EscPosPrinterCommands {
 
     public static byte[][] convertGSv0ToEscAsterisk(byte[] bytes) {
         int
-                xL = bytes[4] & 0xFF,
-                xH = bytes[5] & 0xFF,
-                yL = bytes[6] & 0xFF,
-                yH = bytes[7] & 0xFF,
-                bytesByLine = xH * 256 + xL,
-                dotsByLine = bytesByLine * 8,
-                nH = dotsByLine / 256,
-                nL = dotsByLine % 256,
-                imageHeight = yH * 256 + yL,
-                imageLineHeightCount = (int) Math.ceil((double) imageHeight / 24.0),
-                imageBytesSize = 6 + bytesByLine * 24;
+            xL = bytes[4] & 0xFF,
+            xH = bytes[5] & 0xFF,
+            yL = bytes[6] & 0xFF,
+            yH = bytes[7] & 0xFF,
+            bytesByLine = xH * 256 + xL,
+            dotsByLine = bytesByLine * 8,
+            nH = dotsByLine / 256,
+            nL = dotsByLine % 256,
+            imageHeight = yH * 256 + yL,
+            imageLineHeightCount = (int) Math.ceil((double) imageHeight / 24.0),
+            imageBytesSize = 6 + bytesByLine * 24;
 
         byte[][] returnedBytes = new byte[imageLineHeightCount + 2][];
         returnedBytes[0] = EscPosPrinterCommands.LINE_SPACING_24;
@@ -183,11 +188,11 @@ public class EscPosPrinterCommands {
             imageBytes[4] = (byte) nH;
             for (int j = 5; j < imageBytes.length; ++j) {
                 int
-                        imgByte = j - 5,
-                        byteRow = imgByte % 3,
-                        pxColumn = imgByte / 3,
-                        bitColumn = 1 << (7 - pxColumn % 8),
-                        pxRow = pxBaseRow + byteRow * 8;
+                    imgByte = j - 5,
+                    byteRow = imgByte % 3,
+                    pxColumn = imgByte / 3,
+                    bitColumn = 1 << (7 - pxColumn % 8),
+                    pxRow = pxBaseRow + byteRow * 8;
                 for (int k = 0; k < 8; ++k) {
                     int indexBytes = bytesByLine * (pxRow + k) + pxColumn / 8 + 8;
 
@@ -207,7 +212,6 @@ public class EscPosPrinterCommands {
         returnedBytes[returnedBytes.length - 1] = EscPosPrinterCommands.LINE_SPACING_30;
         return returnedBytes;
     }
-
 
     /**
      * Convert a string to QR Code byte array compatible with ESC/POS printer.
@@ -237,13 +241,13 @@ public class EscPosPrinterCommands {
         }
 
         int
-                width = byteMatrix.getWidth(),
-                height = byteMatrix.getHeight(),
-                coefficient = Math.round((float) size / (float) width),
-                imageWidth = width * coefficient,
-                imageHeight = height * coefficient,
-                bytesByLine = (int) Math.ceil(((float) imageWidth) / 8f),
-                i = 8;
+            width = byteMatrix.getWidth(),
+            height = byteMatrix.getHeight(),
+            coefficient = Math.round((float) size / (float) width),
+            imageWidth = width * coefficient,
+            imageHeight = height * coefficient,
+            bytesByLine = (int) Math.ceil(((float) imageWidth) / 8f),
+            i = 8;
 
         if (coefficient < 1) {
             return EscPosPrinterCommands.initGSv0Command(0, 0);
@@ -534,22 +538,22 @@ public class EscPosPrinterCommands {
             this.printerConnection.write(EscPosPrinterCommands.TEXT_DOUBLE_STRIKE_OFF);
             this.printerConnection.write((":::: Charset n°" + charsetId + " : ").getBytes());
             this.printerConnection.write(new byte[]{
-                    (byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06, (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0A, (byte) 0x0B, (byte) 0x0C, (byte) 0x0D, (byte) 0x0E, (byte) 0x0F,
-                    (byte) 0x10, (byte) 0x11, (byte) 0x12, (byte) 0x13, (byte) 0x14, (byte) 0x15, (byte) 0x16, (byte) 0x17, (byte) 0x18, (byte) 0x19, (byte) 0x1A, (byte) 0x1B, (byte) 0x1C, (byte) 0x1D, (byte) 0x1E, (byte) 0x1F,
-                    (byte) 0x20, (byte) 0x21, (byte) 0x22, (byte) 0x23, (byte) 0x24, (byte) 0x25, (byte) 0x26, (byte) 0x27, (byte) 0x28, (byte) 0x29, (byte) 0x2A, (byte) 0x2B, (byte) 0x2C, (byte) 0x2D, (byte) 0x2E, (byte) 0x2F,
-                    (byte) 0x30, (byte) 0x31, (byte) 0x32, (byte) 0x33, (byte) 0x34, (byte) 0x35, (byte) 0x36, (byte) 0x37, (byte) 0x38, (byte) 0x39, (byte) 0x3A, (byte) 0x3B, (byte) 0x3C, (byte) 0x3D, (byte) 0x3E, (byte) 0x3F,
-                    (byte) 0x40, (byte) 0x41, (byte) 0x42, (byte) 0x43, (byte) 0x44, (byte) 0x45, (byte) 0x46, (byte) 0x47, (byte) 0x48, (byte) 0x49, (byte) 0x4A, (byte) 0x4B, (byte) 0x4C, (byte) 0x4D, (byte) 0x4E, (byte) 0x4F,
-                    (byte) 0x50, (byte) 0x51, (byte) 0x52, (byte) 0x53, (byte) 0x54, (byte) 0x55, (byte) 0x56, (byte) 0x57, (byte) 0x58, (byte) 0x59, (byte) 0x5A, (byte) 0x5B, (byte) 0x5C, (byte) 0x5D, (byte) 0x5E, (byte) 0x5F,
-                    (byte) 0x60, (byte) 0x61, (byte) 0x62, (byte) 0x63, (byte) 0x64, (byte) 0x65, (byte) 0x66, (byte) 0x67, (byte) 0x68, (byte) 0x69, (byte) 0x6A, (byte) 0x6B, (byte) 0x6C, (byte) 0x6D, (byte) 0x6E, (byte) 0x6F,
-                    (byte) 0x70, (byte) 0x71, (byte) 0x72, (byte) 0x73, (byte) 0x74, (byte) 0x75, (byte) 0x76, (byte) 0x77, (byte) 0x78, (byte) 0x79, (byte) 0x7A, (byte) 0x7B, (byte) 0x7C, (byte) 0x7D, (byte) 0x7E, (byte) 0x7F,
-                    (byte) 0x80, (byte) 0x81, (byte) 0x82, (byte) 0x83, (byte) 0x84, (byte) 0x85, (byte) 0x86, (byte) 0x87, (byte) 0x88, (byte) 0x89, (byte) 0x8A, (byte) 0x8B, (byte) 0x8C, (byte) 0x8D, (byte) 0x8E, (byte) 0x8F,
-                    (byte) 0x90, (byte) 0x91, (byte) 0x92, (byte) 0x93, (byte) 0x94, (byte) 0x95, (byte) 0x96, (byte) 0x97, (byte) 0x98, (byte) 0x99, (byte) 0x9A, (byte) 0x9B, (byte) 0x9C, (byte) 0x9D, (byte) 0x9E, (byte) 0x9F,
-                    (byte) 0xA0, (byte) 0xA1, (byte) 0xA2, (byte) 0xA3, (byte) 0xA4, (byte) 0xA5, (byte) 0xA6, (byte) 0xA7, (byte) 0xA8, (byte) 0xA9, (byte) 0xAA, (byte) 0xAB, (byte) 0xAC, (byte) 0xAD, (byte) 0xAE, (byte) 0xAF,
-                    (byte) 0xB0, (byte) 0xB1, (byte) 0xB2, (byte) 0xB3, (byte) 0xB4, (byte) 0xB5, (byte) 0xB6, (byte) 0xB7, (byte) 0xB8, (byte) 0xB9, (byte) 0xBA, (byte) 0xBB, (byte) 0xBC, (byte) 0xBD, (byte) 0xBE, (byte) 0xBF,
-                    (byte) 0xC0, (byte) 0xC1, (byte) 0xC2, (byte) 0xC3, (byte) 0xC4, (byte) 0xC5, (byte) 0xC6, (byte) 0xC7, (byte) 0xC8, (byte) 0xC9, (byte) 0xCA, (byte) 0xCB, (byte) 0xCC, (byte) 0xCD, (byte) 0xCE, (byte) 0xCF,
-                    (byte) 0xD0, (byte) 0xD1, (byte) 0xD2, (byte) 0xD3, (byte) 0xD4, (byte) 0xD5, (byte) 0xD6, (byte) 0xD7, (byte) 0xD8, (byte) 0xD9, (byte) 0xDA, (byte) 0xDB, (byte) 0xDC, (byte) 0xDD, (byte) 0xDE, (byte) 0xDF,
-                    (byte) 0xE0, (byte) 0xE1, (byte) 0xE2, (byte) 0xE3, (byte) 0xE4, (byte) 0xE5, (byte) 0xE6, (byte) 0xE7, (byte) 0xE8, (byte) 0xE9, (byte) 0xEA, (byte) 0xEB, (byte) 0xEC, (byte) 0xED, (byte) 0xEE, (byte) 0xEF,
-                    (byte) 0xF0, (byte) 0xF1, (byte) 0xF2, (byte) 0xF3, (byte) 0xF4, (byte) 0xF5, (byte) 0xF6, (byte) 0xF7, (byte) 0xF8, (byte) 0xF9, (byte) 0xFA, (byte) 0xFB, (byte) 0xFC, (byte) 0xFD, (byte) 0xFE, (byte) 0xFF
+                (byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06, (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0A, (byte) 0x0B, (byte) 0x0C, (byte) 0x0D, (byte) 0x0E, (byte) 0x0F,
+                (byte) 0x10, (byte) 0x11, (byte) 0x12, (byte) 0x13, (byte) 0x14, (byte) 0x15, (byte) 0x16, (byte) 0x17, (byte) 0x18, (byte) 0x19, (byte) 0x1A, (byte) 0x1B, (byte) 0x1C, (byte) 0x1D, (byte) 0x1E, (byte) 0x1F,
+                (byte) 0x20, (byte) 0x21, (byte) 0x22, (byte) 0x23, (byte) 0x24, (byte) 0x25, (byte) 0x26, (byte) 0x27, (byte) 0x28, (byte) 0x29, (byte) 0x2A, (byte) 0x2B, (byte) 0x2C, (byte) 0x2D, (byte) 0x2E, (byte) 0x2F,
+                (byte) 0x30, (byte) 0x31, (byte) 0x32, (byte) 0x33, (byte) 0x34, (byte) 0x35, (byte) 0x36, (byte) 0x37, (byte) 0x38, (byte) 0x39, (byte) 0x3A, (byte) 0x3B, (byte) 0x3C, (byte) 0x3D, (byte) 0x3E, (byte) 0x3F,
+                (byte) 0x40, (byte) 0x41, (byte) 0x42, (byte) 0x43, (byte) 0x44, (byte) 0x45, (byte) 0x46, (byte) 0x47, (byte) 0x48, (byte) 0x49, (byte) 0x4A, (byte) 0x4B, (byte) 0x4C, (byte) 0x4D, (byte) 0x4E, (byte) 0x4F,
+                (byte) 0x50, (byte) 0x51, (byte) 0x52, (byte) 0x53, (byte) 0x54, (byte) 0x55, (byte) 0x56, (byte) 0x57, (byte) 0x58, (byte) 0x59, (byte) 0x5A, (byte) 0x5B, (byte) 0x5C, (byte) 0x5D, (byte) 0x5E, (byte) 0x5F,
+                (byte) 0x60, (byte) 0x61, (byte) 0x62, (byte) 0x63, (byte) 0x64, (byte) 0x65, (byte) 0x66, (byte) 0x67, (byte) 0x68, (byte) 0x69, (byte) 0x6A, (byte) 0x6B, (byte) 0x6C, (byte) 0x6D, (byte) 0x6E, (byte) 0x6F,
+                (byte) 0x70, (byte) 0x71, (byte) 0x72, (byte) 0x73, (byte) 0x74, (byte) 0x75, (byte) 0x76, (byte) 0x77, (byte) 0x78, (byte) 0x79, (byte) 0x7A, (byte) 0x7B, (byte) 0x7C, (byte) 0x7D, (byte) 0x7E, (byte) 0x7F,
+                (byte) 0x80, (byte) 0x81, (byte) 0x82, (byte) 0x83, (byte) 0x84, (byte) 0x85, (byte) 0x86, (byte) 0x87, (byte) 0x88, (byte) 0x89, (byte) 0x8A, (byte) 0x8B, (byte) 0x8C, (byte) 0x8D, (byte) 0x8E, (byte) 0x8F,
+                (byte) 0x90, (byte) 0x91, (byte) 0x92, (byte) 0x93, (byte) 0x94, (byte) 0x95, (byte) 0x96, (byte) 0x97, (byte) 0x98, (byte) 0x99, (byte) 0x9A, (byte) 0x9B, (byte) 0x9C, (byte) 0x9D, (byte) 0x9E, (byte) 0x9F,
+                (byte) 0xA0, (byte) 0xA1, (byte) 0xA2, (byte) 0xA3, (byte) 0xA4, (byte) 0xA5, (byte) 0xA6, (byte) 0xA7, (byte) 0xA8, (byte) 0xA9, (byte) 0xAA, (byte) 0xAB, (byte) 0xAC, (byte) 0xAD, (byte) 0xAE, (byte) 0xAF,
+                (byte) 0xB0, (byte) 0xB1, (byte) 0xB2, (byte) 0xB3, (byte) 0xB4, (byte) 0xB5, (byte) 0xB6, (byte) 0xB7, (byte) 0xB8, (byte) 0xB9, (byte) 0xBA, (byte) 0xBB, (byte) 0xBC, (byte) 0xBD, (byte) 0xBE, (byte) 0xBF,
+                (byte) 0xC0, (byte) 0xC1, (byte) 0xC2, (byte) 0xC3, (byte) 0xC4, (byte) 0xC5, (byte) 0xC6, (byte) 0xC7, (byte) 0xC8, (byte) 0xC9, (byte) 0xCA, (byte) 0xCB, (byte) 0xCC, (byte) 0xCD, (byte) 0xCE, (byte) 0xCF,
+                (byte) 0xD0, (byte) 0xD1, (byte) 0xD2, (byte) 0xD3, (byte) 0xD4, (byte) 0xD5, (byte) 0xD6, (byte) 0xD7, (byte) 0xD8, (byte) 0xD9, (byte) 0xDA, (byte) 0xDB, (byte) 0xDC, (byte) 0xDD, (byte) 0xDE, (byte) 0xDF,
+                (byte) 0xE0, (byte) 0xE1, (byte) 0xE2, (byte) 0xE3, (byte) 0xE4, (byte) 0xE5, (byte) 0xE6, (byte) 0xE7, (byte) 0xE8, (byte) 0xE9, (byte) 0xEA, (byte) 0xEB, (byte) 0xEC, (byte) 0xED, (byte) 0xEE, (byte) 0xEF,
+                (byte) 0xF0, (byte) 0xF1, (byte) 0xF2, (byte) 0xF3, (byte) 0xF4, (byte) 0xF5, (byte) 0xF6, (byte) 0xF7, (byte) 0xF8, (byte) 0xF9, (byte) 0xFA, (byte) 0xFB, (byte) 0xFC, (byte) 0xFD, (byte) 0xFE, (byte) 0xFF
             });
             this.printerConnection.write(new byte[]{EscPosPrinterCommands.LF, EscPosPrinterCommands.LF, EscPosPrinterCommands.LF, EscPosPrinterCommands.LF});
             this.printerConnection.send();
@@ -581,9 +585,10 @@ public class EscPosPrinterCommands {
         if (!this.printerConnection.isConnected()) {
             return this;
         }
-        byte[][] bytesToPrint = this.useEscAsteriskCommand ? EscPosPrinterCommands.convertGSv0ToEscAsterisk(image) : new byte[][] {image};
 
-        for(byte[] bytes : bytesToPrint) {
+        byte[][] bytesToPrint = this.useEscAsteriskCommand ? EscPosPrinterCommands.convertGSv0ToEscAsterisk(image) : new byte[][]{image};
+
+        for (byte[] bytes : bytesToPrint) {
             this.printerConnection.write(bytes);
             this.printerConnection.send();
         }
@@ -642,9 +647,9 @@ public class EscPosPrinterCommands {
             byte[] textBytes = text.getBytes("UTF-8");
 
             int
-                    commandLength = textBytes.length + 3,
-                    pL = commandLength % 256,
-                    pH = commandLength / 256;
+                commandLength = textBytes.length + 3,
+                pL = commandLength % 256,
+                pH = commandLength / 256;
 
             /*byte[] qrCodeCommand = new byte[textBytes.length + 7];
             System.arraycopy(new byte[]{0x1B, 0x5A, 0x00, 0x00, (byte)size, (byte)pL, (byte)pH}, 0, qrCodeCommand, 0, 7);
@@ -741,7 +746,6 @@ public class EscPosPrinterCommands {
         }
 
         this.printerConnection.write(new byte[]{0x1B, 0x70, 0x00, 0x3C, (byte) 0xFF});
-//        this.printerConnection.write(new byte[]{0x1B, 0x70, 0x0, 0x64, (byte) 0xFA});
         this.printerConnection.send(100);
         return this;
     }
